1d7db9eec922380817ae28106e011335c991e571
soumyagangamwar <soumyagangamwar@gmail.com>
2022-12-06 13:25:25 +0530

    Loader added

--- assignments/src/components/cards/CardContainer.js
+++ assignments/src/components/cards/CardContainer.js
@@ -1,8 +1,9 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import CardComponent from "./Cards.js";
-import NoData from "../../shared/noData.js"
+import NoData from "../../shared/NoData.js";
+import Loader from "../../shared/Loader.js";
 import SearchComponent from "../../shared/Search.js";
-import fetchservice from '../../services/fetch.service.js';
+import fetchservice from "../../services/fetch.service.js";
 import "./cards.css";
 
 const gitUserNames = fetchservice();
@@ -12,36 +13,41 @@ const CardContainer = () => {
   const [filteredTeamMembers, setfilteredTeamMembers] = useState([]);
   const [teamMembersdata, setteamMembersdata] = useState([]);
   const [searchText, setSearchText] = useState("");
-  
-  useEffect(()=> {
-    gitUserNames.then(res=> {
-      const data =[].concat(...res);
-     // console.log(res, '*****', data)
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    gitUserNames.then((res) => {
+      const data = [].concat(...res);
+      // console.log(res, '*****', data)
       setteamMembersdata(data);
-      setfilteredTeamMembers(data)
-    })
-  }, [])
-   
+      setfilteredTeamMembers(data);
+
+      setTimeout(() => {
+      setloading(false);
+      }, 5000);
+    });
+  }, []);
+
   return (
-   <>
-    <SearchComponent setfilteredTeamMembers = {setfilteredTeamMembers}
-                     teamMembersdata = {teamMembersdata} 
-                     searchText = {searchText}
-                     setSearchText={setSearchText}/>
-    <div className="card-container">
-      {
-        filteredTeamMembers == ""  ? (
-         <NoData/>
-         ): (
-          filteredTeamMembers.map((data) => ( 
+    <>
+      <SearchComponent
+        setfilteredTeamMembers={setfilteredTeamMembers}
+        teamMembersdata={teamMembersdata}
+        searchText={searchText}
+        setSearchText={setSearchText}
+      />
+      <div className="card-container">
+        {loading ? (
+          <Loader />
+        ) : filteredTeamMembers == "" ? (
+          <NoData />
+        ) : (
+          filteredTeamMembers.map((data) => (
             <CardComponent teamData={data} key={data.id} />
-            ))
-         )
-      }
-     </div>
-   </>
-     
-    
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
